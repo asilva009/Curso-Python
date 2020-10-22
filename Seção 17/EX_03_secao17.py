c252e410a54125552ac2147b05fbfbcd673d1395
asilva009 <asilva009@yahoo.com.br>
2020-10-21 21:18:41 -0300

    correção EX_03_secao17

--- Seção 17/EX_03_secao17.py
+++ Seção 17/EX_03_secao17.py
@@ -1,22 +1,21 @@
 class Quadrado:
 
-    area, perimetro = 0, 0
-
     def __init__(self, lado):
         self.__lado = lado
+        self.__area = self.calcular_area()
+        self.__perimetro = self.calcular_perimetro()
 
     def calcular_area(self):
-        Quadrado.area = self.__lado * self.__lado
+        return self.__lado * self.__lado
 
     def calcular_perimetro(self):
-        Quadrado.perimetro = self.__lado * 4
+        return self.__lado * 4
 
     def imprimir(self):
-        return f'O quadrado de lado {self.__lado} tem área {Quadrado.area} e perimetro {Quadrado.perimetro}'
+        return f'O quadrado de lado {self.__lado} tem área {self.__area} e perimetro {self.__perimetro}'
+
 
 q1 = Quadrado(5)
-q1.calcular_area()
-q1.calcular_perimetro()
 print(q1.imprimir())  # O quadrado de lado 5 tem área 25 e perimetro 20
 
 
